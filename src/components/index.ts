9a1cd4f4225c035e20d046de5dad2e83b752eb51
AI Website Generator <generator@yourplatform.com>
2025-09-13 16:03:30 +0000

    Initial website generation

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Navigation } from '@/components/layout/Navigation'
+export { default as Footer } from '@/components/layout/Footer'
+export { default as Hero } from '@/components/sections/Hero'
